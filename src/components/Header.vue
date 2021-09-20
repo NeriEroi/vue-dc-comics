f1b547a41bf84cde765da650296aa2af49cd3f3a
Michele <micheleierniero@gmail.com>
2021-09-20 14:09:09 +0200

    scaffolding header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>
+          Header correttamente impostato
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
